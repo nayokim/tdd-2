8b670ce7990c8b006f1adeba59d1e88afd80833e
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-07-31 09:08:33 -0500

    practice 2

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -4,36 +4,38 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class StudentTest {
+
     Student nayo;
+    Student studentWithManyGrades;
 
     @Before
-    public void setup(){
-        nayo = new Student (1, "Nayoung");
+    public void setUp(){
+        nayo = new Student(1, "Nayoung");
         nayo.addGrade(90);
-        nayo.addGrade(80);
-        nayo.addGrade(70);
+        nayo.addGrade(90);
+        nayo.addGrade(90);
     }
 
     @Test
-    public void ifUserObjectCanBeCreated(){
-        assertEquals("Nayoung", nayo.getName());
-    }
-
-    @Test
-    public void ifUserIdIsCorrect(){
+    public void testId(){
         assertEquals(1, nayo.getId());
     }
 
     @Test
-    public void userAddGrade(){
-        assertSame(90,  nayo.getGrades().get(0));
+    public void testName(){
+        assertEquals("Nayoung", nayo.getName());
     }
 
     @Test
-    public void testIfAvgCorrect(){
-        assertEquals(90, nayo.getAverage(),10 );
+    public void testAddGrade(){
+        assertSame(90,nayo.getGrades().get(0));
     }
 
+
+    @Test
+    public void testAverages(){
+        assertEquals(90,nayo.getAverage(),0);
+    }
 
 
 }
