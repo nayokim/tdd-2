6cfa8f793a5086ff286092ebd77184b2a564c776
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-07-31 07:51:16 -0500

    blank master

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,45 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-    private long id;
-    private String name;
-    private ArrayList<Integer> grades;
-
-
-    public Student (long id, String name){
-        this. id = id;
-        this. name = name;
-        this.grades = new ArrayList<>();
-
-    }
-
-
-    public String getName(){
-         return this.name;
-    }
-
-    public long getId(){
-        return this.id;
-    }
-
-    public void addGrade(int grade){
-        this.grades.add(grade);
-    }
-
-    public ArrayList<Integer> getGrades(){
-       return this.grades;
-    }
-
-    public double getAverage(){
-        double result = 0;
-        for (double grade : grades){
-            result += grade;
-        }
-        return result / getGrades().size();
-    }
-
-
-
 
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -4,36 +4,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class StudentTest {
-    Student nayo;
-
-    @Before
-    public void setup(){
-        nayo = new Student (1, "Nayoung");
-        nayo.addGrade(90);
-        nayo.addGrade(80);
-        nayo.addGrade(70);
-    }
-
-    @Test
-    public void ifUserObjectCanBeCreated(){
-        assertEquals("Nayoung", nayo.getName());
-    }
-
-    @Test
-    public void ifUserIdIsCorrect(){
-        assertEquals(1, nayo.getId());
-    }
-
-    @Test
-    public void userAddGrade(){
-        assertSame(90,  nayo.getGrades().get(0));
-    }
-
-    @Test
-    public void testIfAvgCorrect(){
-        assertEquals(90, nayo.getAverage(),10 );
-    }
-
 
 
 }
