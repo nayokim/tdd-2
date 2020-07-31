228d6788e11e53786b29b8058d5fd33ace3c9114
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-07-31 08:19:59 -0500

    junit tests

--- src/test/java/CirrTest.java
+++ src/test/java/CirrTest.java
@@ -8,103 +8,132 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+//Create a test method that uses assertEquals or assertNotEquals to verify if the String Codeup is the same as CodeUp.
+//Create a test method that uses assertNotSame to verify that the following ArrayLists are not the same:
+//
+//List<String> languages = new ArrayList<>();
+//List<String> moreLanguages = new ArrayList<>();
+//Create a test method that uses assertArrayEquals to verify the following arrays are equal.
+//
+//int[] numbers = {1, 2, 3};
+//int[] otherNumbers = new int[3];
+//otherNumbers[0] = 1;
+//otherNumbers[1] = 2;
+//otherNumbers[2] = 3;
+//Create a test method that uses both assertFalse and assertTrue to verify the following statements:
+//
+//String language = "PHP";
+//language.contains("H"); // use assertTrue for this statement
+//language.contains("J"); // use assertFalse for this statement
 public class CirrTest {
+
     @Test
-    public void testIfCompanyNameIsEquals(){
+    public void assertEqualsStrings(){
         String expected = "Codeup";
-        String actual = "Codeup";
+        String actual = "CodeUp";
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
-    public void testIfCompanyisEqual(){
-        String expected = "codeup";
-        String actual = "Codeup";
-        //expected to fail
-        assertEquals(expected,actual);
+    public void assertNotEqualsStrings(){
+        String expected = "Codeup";
+        String actual = "CodeUp";
+
+        assertNotEquals(expected,actual);
+    }
+
+    //Create a test method that uses assertNotSame to verify that the following ArrayLists are not the same:
+//
+//List<String> languages = new ArrayList<>();
+//List<String> moreLanguages = new ArrayList<>();
+//Create a test method that uses assertArrayEquals to verify the following arrays are equal.
+//
+
+    @Test
+    public void assertNotSameArrayList(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
     }
 
     @Test
-    public void testIfChangeIsCorrect(){
-        Double price = 10.0;
-        Double discount = 4.5;
-
-        //1. write a failing test
-//        assertEquals(10.5, price-discount, 0);
-        //2. refactor
-        assertEquals(5.5, price-discount, 0);
-        assertEquals(5.1, price-discount,  0.5);
-        assertEquals(4.9, price-discount, 0.6);
+    public void assertSameArrayList(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+        assertSame(languages, moreLanguages);
     }
 
-    //assertNotSame - its possible to verify if two variables dont refer to the same object
-    //assertSame - otherwise, we want to verify that two variables refer to the same object
+    // the difference between assertSame vs assertEquals
+    //assertEquals: asserts that objects are equals
+    //assertSame : 2 objects refer to the same object
+    // @Test
+//    public void testAssertions(){
+//
+//        Student s1 = new Student();
+//        s1.name = "watson";
+//        Student s2 = s1;
+//
+//        //below assertions is successful as s1 and s2 point to same object
+//        Assert.assertEquals(s1,s2);
+//
+//        //below assertions is successful as s1 and s2 point to same object
+//        Assert.assertSame(s1, s2);
+//
+//        Student s3 = new Student();
+//        s3.name = "Shaun";
+//
+//        //below assertions is unsuccessful as equals method of s1 returns false
+//        Assert.assertEquals(s1,s3);
+//
+//        //below assertions is unsuccessful as s1 and s3 point to different object
+//        Assert.assertSame(s1,s3);
+//
+//        s3.name = "watson";
+//        //below assertions is successful as equals method of s1 returns true
+//        Assert.assertEquals(s1,s3);
+//
+//        //below assertions is unsuccessful as s1 and s3 are different objects
+//        Assert.assertSame(s1,s3);
+//    }
+
+    //Create a test method that uses assertArrayEquals to verify the following arrays are equal.
+//
+//int[] numbers = {1, 2, 3};
+//int[] otherNumbers = new int[3];
+//otherNumbers[0] = 1;
+//otherNumbers[1] = 2;
+//otherNumbers[2] = 3;
 
     @Test
-    public void testIfObjectsAreDifferent(){
-        Object dog = new Object();
-        Object sheep = new Object();
-        Object clonnedSheep = sheep;
+    public void assertArrayEqualsNumbers(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
 
-//        assertNotSame(sheep, dog);
-//        assertNotSame(sheep,clonnedSheep);
-//        assertSame(clonnedSheep,sheep); // they are referring to the same object
-//        assertSame(dog, clonnedSheep);
+        assertArrayEquals(numbers, otherNumbers);
     }
 
-    //assertArrayEquals - method checks that two object arrays are equal or not
+    //Create a test method that uses both assertFalse and assertTrue to verify the following statements:
+//
+//String language = "PHP";
+//language.contains("H"); // use assertTrue for this statement
+//language.contains("J"); // use assertFalse for this statement
     @Test
-    public void  testIfArrayEquals1(){
-        char [] expected =  {'J','u','n','i','t'};
-        char  [] actual = "Junit".toCharArray();
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void testIfArrayEquals2(){
-        char [] expected = {'n','a','y','o'};
-        char [] actual = {'n'};
-
-        assertArrayEquals(expected,actual);
-    }
-
-    //assert true or false : assertTrue and assertFalse
-    @Test
-    public void testIfGreaterOrLesserThanWorks(){
-        boolean learningTDD = true;
-
-        assertTrue(learningTDD);
-        assertTrue("5 is greater than 4", 5>4);
-        assertTrue("4 is less than 5", 4<5);
-        assertFalse("5 is not greater than 6", 5 >6);
-    }
-
-
-    private List<String> names;
-    //@Before annotation: methods of superclasses that will run before those of the current class
-    @Before
-    public void setUp(){
-        this.names = new ArrayList<>();
-        this.names.add("Fer"); // ["Fer"]
+    public void testAssertTrue(){
+        String language = "PHP";
+        assertTrue(language.contains("H"));
     }
 
     @Test
-    public void testIfNamesIsInitialized(){
-        assertNotNull(names);
+    public void testAssertFalse(){
+        String language = "PHP";
+        assertFalse(language.contains("J"));
     }
 
-    @Test
-    public void testIfANameCanBeAdded(){
-        assertEquals(1, this.names.size());
-//        ["Fer"]
-        this.names.add("Zach"); // adding zach
-        System.out.println(names);//["Fer", "Zach"]
-        assertEquals(2,this.names.size()); // size is not 2
-        assertSame("Fer", this.names.get(0));
-        assertSame("Zach", this.names.get(1));
-    }
 
 
 }
